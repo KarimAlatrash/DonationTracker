b6bf2fcb6b3473b441978f4e4295dd0e6b85cf0e
KarimAlatrash <karimalatrash@gmail.com>
2021-04-28 16:15:08 -0400

    commit for vercel

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,30 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import Providers from "next-auth/providers";
+import Adapters from "next-auth/adapters";
+import { NextApiHandler } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options:NextAuthOptions = {
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Providers.Google({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    
+  ],
+  // @ts-ignore
+  adapter: Adapters.Prisma.Adapter({
+    prisma,
+  }),
+
+  secret: process.env.SECRET,
+};
